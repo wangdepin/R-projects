661fb204e2d9997cee38f5edccb1cdde2e39226d
wangdepin <wangdepin5135@gmail.com>
2021-04-20 10:36:16 +0800

    add R script

---
+++ R/download_data__from_TCGA.R
@@ -0,0 +1,14 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+if (!requireNamespace("ComplexHeatmap", quietly = TRUE))
+  BiocManager::install("ComplexHeatmap") # from bioconductor
+
+if (!requireNamespace("TCGAbiolinks", quietly = TRUE))
+  BiocManager::install("BioinformaticsFMRP/TCGAbiolinks") # from github
+
+library(TCGAbiolinks)
+library(MultiAssayExperiment)
+library(maftools)
+library(dplyr)
+library(ComplexHeatmap)
